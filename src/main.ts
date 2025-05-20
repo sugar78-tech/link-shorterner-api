c73f61d8764f83b461d4c2eae342604b45cf12c5
eduvilla97 <eduvilla99@gmail.com>
2025-05-20 19:59:48 +0200

    sys: env log for deploy before app init

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
   console.log(process.env);
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: process.env.FRONTEND_URL,
   });
