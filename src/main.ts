ca41670eab92868e7dd7992e7ff333ba43f39f98
eduvilla97 <eduvilla99@gmail.com>
2025-05-20 20:37:38 +0200

    sys: enable cors and remove log

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log(process.env);
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
