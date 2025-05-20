746d3d5bc9f6d4c4c0994327e6f46b716923f2c9
eduvilla97 <eduvilla99@gmail.com>
2025-05-20 19:27:20 +0200

    feat: database integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import { LinksModule } from './links/links.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Link } from './links/entities/link.entity';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    LinksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? '5432'),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [Link],
+      synchronize: process.env.NODE_ENV === 'development',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
