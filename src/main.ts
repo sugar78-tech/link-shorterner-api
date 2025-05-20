e4c0e71c3d36b31e10f6ace88acb4f5d60def073
eduvilla97 <eduvilla99@gmail.com>
2025-05-20 19:43:28 +0200

    sys: enable cors globaly for dev and pro

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  if (process.env.NODE_ENV === 'development') {
-    app.enableCors();
-  }
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+  });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
