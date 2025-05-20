038dfd23dafccd4751b6021273b435e541591ea0
eduvilla97 <eduvilla99@gmail.com>
2025-05-20 19:48:58 +0200

    sys: env log for deploy

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log(process.env);
   app.enableCors({
     origin: process.env.FRONTEND_URL,
   });
